e9ae0307c5c29c184f4b7ff469c9f77cd850b2b8
Erwan Moreau <erwan.moreau.ouest@gmail.com>
2020-09-03 18:30:48 +0200

    Command system

---
+++ bin/jasmine-esm.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+
+import path from 'path';
+import Command from '../lib/command.js';
+import JasmineEsm from '../jasmine-esm.js';
+
+(async () => {
+const jasmineEsm = new JasmineEsm({ projectBaseDir: path.resolve() });
+const examplesDir = path.join(path.dirname(await import.meta.resolve('jasmine-core')), 'jasmine-core', 'example', 'node_example');
+const command = new Command(path.resolve(), examplesDir, console.log);
+
+command.run(jasmineEsm, process.argv.slice(2));
+})();
